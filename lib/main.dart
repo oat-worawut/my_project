25c9e230c8df633037de8efca1730baeff0ba7fc
oat.worawut <oat.worawut@maqe.com>
2022-03-25 17:03:33 +0700

    update file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  // FirebaseCrashlytics.instance.crash();
   runApp(const MyApp());
 }
 
